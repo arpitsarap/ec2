82f228b00dbb6a25955dd20933ad68eef758b3ea
arpitsarap <arpit.s@protectonce.com>
2023-12-28 14:34:34 +0530

    add

--- ec2.sh
+++ ec2.sh
@@ -1,15 +1,16 @@
 sudo su
-sudo yum install nginx
-sudo yum install docker
-sudo systemctl start nginx
-sudo service docker start
-sudo docker run -d -p 3000:3000 bkimminich/juice-shop
-sudo yum install git
+yum  install docker
+systemctl start docker
+yum install nginx
+systemctl start nginx
 cd /
-cd home
-sudo git clone https://github.com/arpitsarap/ec2.git
+cd home/
+ls
 cd ec2
-sudo cp libngx_http_protectonce_mirror_module.so /usr/share/nginx/modules/
-sudo cp nginx_confix_ec2 /etc/nginx/nginx.conf
-sudo systemctl reload nginx
+cd 1.24
+cp libngx_http_protectonce_mirror_module.so /usr/share/nginx/modules/
+cd ..
+cp nginx_confix_ec2 /etc/nginx/nginx.conf
+docker run -it -d -p 3001:3000 -e PORT=5000 pradyum619/users-express-app
 
+echo "Change the nginx.conf as per req."
